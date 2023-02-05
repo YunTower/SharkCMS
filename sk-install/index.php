f0ecc77767519f1f6ea5e73b3169bf9475865f2c
fish_nb <286267038@qq.com>
2023-02-06 02:14:08 +0800

    dev 2023/2/6 增加后台鉴权&登陆功能&管理员信息加密&安装时邮箱合法性验证

--- sk-install/index.php
+++ sk-install/index.php
@@ -21,25 +21,25 @@
 			</div>
 		</div>
 		<div class="layui-form-item">
-			<input type="text" placeholder="数据库地址" value="127.0.0.1" name="dbhost" lay-verify="required" hover class="layui-input" />
+			<input type="text" placeholder="数据库地址" value="" name="dbhost" lay-verify="required" hover class="layui-input" />
 		</div>
 		<div class="layui-form-item">
-			<input type="text" placeholder="数据库名称" value="test" name="dbname" lay-verify="required" hover class="layui-input" />
+			<input type="text" placeholder="数据库名称" value="" name="dbname" lay-verify="required" hover class="layui-input" />
 		</div>
 		<div class="layui-form-item">
-			<input type="text" placeholder="数据库账号" value="test" name="dbuser" lay-verify="required" hover class="layui-input" />
+			<input type="text" placeholder="数据库账号" value="" name="dbuser" lay-verify="required" hover class="layui-input" />
 		</div>
 		<div class="layui-form-item">
-			<input type="text" placeholder="数据库密码" value="testtest" name="dbpwd" lay-verify="required" hover class="layui-input" />
+			<input type="text" placeholder="数据库密码" value="" name="dbpwd" lay-verify="required" hover class="layui-input" />
 		</div>
 		<div class="layui-form-item">
-			<input type="email" placeholder="管理员邮箱" id="mail" value="286267038@qq.com" name="adminmail" lay-verify="required" hover class="layui-input" />
+			<input type="email" placeholder="管理员邮箱" id="mail" value="" name="adminmail" lay-verify="required|email" hover class="layui-input" />
 		</div>
 		<div class="layui-form-item">
-			<input type="text" placeholder="管理员账号" value="test" name="adminname" lay-verify="required" hover class="layui-input" />
+			<input type="text" placeholder="管理员账号" value="" name="adminname" lay-verify="required" hover class="layui-input" />
 		</div>
 		<div class="layui-form-item">
-			<input type="password" placeholder="管理员密码" id="pwd" value="testtest" name="adminpwd" lay-verify="required" hover class="layui-input" />
+			<input type="password" placeholder="管理员密码" id="pwd" value="" name="adminpwd" lay-verify="required" hover class="layui-input" />
 		</div>
 		<div class="layui-form-item">
 			<button class="pear-btn pear-btn-success login" lay-submit lay-filter="login">
@@ -62,30 +62,6 @@
         });
       }
     }
-		layui.use(['form', 'button', 'popup'], function() {
-			var form = layui.form;
-			var button = layui.button;
-			var popup = layui.popup;
-
-			// 登 录 提 交
-			form.on('submit(login)', function() {
-				var mail = $('#mail').val();
-				var pwd = $('#pwd').val();
-				// 验证
-
-				if (mail.match(/^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/)) {
-					if (pwd.length < 6) {
-						layer.alert("【管理员密码】少于6位！");
-						return false
-					} else{
-						layer.msg('请求安装中...')
-					}
-				} else {
-					layer.alert('【邮箱】格式不合法！')
-					return false
-				}
-			});
-		})
 	</script>
 </body>
 
